fa984bc36629be76fe153ed5b3f3707a64934e06
Roman Kovtunov <ro.kovtunov@mail.ru>
2023-11-30 15:31:30 +0300

    Первый полет в космос, шучу,  на морду

--- smart_ui/src/app/app.component.ts
+++ smart_ui/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { DxButtonModule } from 'devextreme-angular';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, DxButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'smart_ui';
+
+  public helloWorld(): void {
+    alert("Hello World!")
+  }
 }
